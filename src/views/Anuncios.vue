5f95abb1cb8a87bc09490736d7be0d5104cd0808
josuec4321 <josuec4321@gmail.com>
2020-07-31 16:53:31 -0500

    unificando router vie com masterpage

---
+++ src/views/Anuncios.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="cont">
+         <h5>Anuncios</h5>
+      <hr>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="card rounded-0 p-0 shadow-sm">
+            <img src="https://user-images.githubusercontent.com/25878302/58369568-a49b2480-7efc-11e9-9ca9-2be44afacda1.png" class="card-img-top rounded-0" alt="Angular pro sidebar">
+            <div class="card-body text-center">
+              <h6 class="card-title">Poesia y canto</h6>
+              <a href="https://github.com/azouaoui-med/angular-pro-sidebar" target="_blank" class="btn btn-primary btn-sm">Ver más</a>
+              <!-- <a href="https://azouaoui-med.github.io/angular-pro-sidebar/demo/" target="_blank" class="btn btn-success btn-sm">Demo</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="card rounded-0 p-0 shadow-sm">
+            <img src="https://user-images.githubusercontent.com/25878302/58369258-33f20900-7ef8-11e9-8ff3-b277cb7ed7b4.PNG" class="card-img-top rounded-0" alt="Angular pro sidebar">
+            <div class="card-body text-center">
+              <h6 class="card-title">HeroGame</h6>
+              <a href="https://github.com/azouaoui-med/lightning-admin-angular" target="_blank" class="btn btn-primary btn-sm">Ver más</a>
+              <!-- <a href="https://azouaoui-med.github.io/lightning-admin-angular/demo/" target="_blank" class="btn btn-success btn-sm">Demo</a> -->
+              
+            </div>
+          </div>
+        </div>
+      
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="card rounded-0 p-0 shadow-sm">
+            <img src="https://user-images.githubusercontent.com/25878302/58369568-a49b2480-7efc-11e9-9ca9-2be44afacda1.png" class="card-img-top rounded-0" alt="Angular pro sidebar">
+            <div class="card-body text-center">
+              <h6 class="card-title">Pasion de gavilanes Teatro</h6>
+              <a href="https://github.com/azouaoui-med/angular-pro-sidebar" target="_blank" class="btn btn-primary btn-sm">Ver más</a>
+              <!-- <a href="https://azouaoui-med.github.io/angular-pro-sidebar/demo/" target="_blank" class="btn btn-success btn-sm">Demo</a> -->
+             
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <hr>
+    </div>
+</template>
+<script>
+export default {
+    name: 'anuncios',
+}
+</script>
+<style scoped>
+    .cont{
+        display: flex;
+        flex-direction: column;
+        height: 1000px;
+    }
+</style>
---
+++ src/views/mainpage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hola esta es la vista general</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    HelloWorld
+  }
+}
+</script>
