d5f67d4b0f0d7118aab358cdfcd6a72d017f1d15
Gustavo Troncos <gustavopoth@gmail.com>
2020-08-02 07:59:34 -0500

    hi

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,10 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Alumno/game.vue')
+  },{
+    path: '/Docente/evaluacion',
+    name: 'evaluacion',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Docente/evaluacion.vue')
   }
 ]
 
