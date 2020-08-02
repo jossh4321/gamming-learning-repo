53ebd5e83fdbe1da1782ee79370ce96574cb420b
josuec4321 <josuec4321@gmail.com>
2020-08-02 07:57:16 -0500

    resolucion de conflictos

--- src/views/Alumno/game.vue
+++ src/views/Alumno/game.vue
@@ -18,7 +18,6 @@
         </v-row>
       </v-col>
 
-<<<<<<< HEAD
       <v-col cols="7">
         <v-row>
           <v-col cols="4">
@@ -163,176 +162,6 @@
                 </v-row>
               </v-col>
             </v-row>
-=======
-      <v-row>
-        <v-col>
-          <v-row>
-            <v-col>
-              <v-card class="item" outlined>
-                <v-btn
-                  color="blue-grey"
-                  class="ma-2 white--text"
-                  width="100%"
-                  height="100%"
-                  @click="dialogoTienda = true">
-                  <v-icon right dark>mdi-gamepad-variant-outline</v-icon>
-                  <span style="margin-left:20px">Tienda</span>
-                </v-btn>
-              </v-card>
-              <v-dialog v-model="dialogoTienda" persistent max-width="750px">
-                  <v-card>
-                    <v-card-title class="headline">Tienda</v-card-title>
-                    <v-item-group multiple>
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card
-          class="mx-auto"
-          max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="../../assets/img/Tienda/espadalu.jpg"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Espada Luminosa</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-        <div>Espada relusiente con un poder del Sol</div>
-        <div>Strength: 300 , Anger: 80, Speed: 100</div>
-        </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card
-          class="mx-auto"
-          max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="../../assets/img/Tienda/guantescaballero.png"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Guantes de Caballero</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-        <div>Guantes de caballero de plata</div>
-        <div>Strength: 500 , Anger: 50, Speed: 40</div>
-        </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card
-          class="mx-auto"
-          max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="../../assets/img/Tienda/botas.jpg"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Botas de Caballero</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-        <div>Botas de Caballero resistente al caminar</div>
-        <div>Strength: 20 , Skill: 20, Speed: 300</div>
-        </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card
-          class="mx-auto"
-          max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="../../assets/img/Tienda/escudo.jpg"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Escudo de roble</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-        <div>Un escudo hecho de madera, bloquea algunos ataques</div>
-        <div>Skill: 130 , Strength: 50, Health: 300</div>
-        </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card
-          class="mx-auto"
-          max-width="400">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="../../assets/img/Tienda/casco.jpg"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Casco medieval</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-        <div>Un casco que permite proteger contra ataques a la cabeza</div>
-        <div>Strength: 350 , Anger: 60, Health: 100</div>
-        </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-item-group>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-col>
-                      <v-btn color="green darken-1" @click="dialogoTienda = false">Aceptar</v-btn>
-                      </v-col>
-                      <v-col>
-                      <v-btn color="green darken-1" @click="dialogoTienda = false">Cancelar</v-btn>
-                      </v-col>
-                    </v-card-actions>
-                  </v-card>
-
-                  
-              </v-dialog>
-            </v-col>
-            <v-col>
-              <v-card class="item" outlined>
-                <v-btn color="blue-grey" class="ma-2 white--text" width="100%" height="100%" @click="dialogoHeroes = true">
-                  <v-icon right dark>mdi-gamepad-variant-outline</v-icon>
-                  <span style="margin-left:20px">Heroes</span>
-                </v-btn>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card class="item" outlined>
-                <v-btn color="blue-grey" class="ma-2 white--text" width="100%" height="100%" @click="dialogoMonedero = true">
-                  <v-icon right dark>mdi-gamepad-variant-outline</v-icon>
-                  <span style="margin-left:20px">Monedero</span>
-                </v-btn>
-              </v-card>
-            </v-col>
->>>>>>> 815f9f11f7c14285ffcf95271823dd199192e12f
 
             </v-card>
 
@@ -359,9 +188,6 @@
     <v-dialog v-model="dialogoHeroes" width="500px" heigth="500px">
       <v-card>
         <v-card-title class="headline">Heores</v-card-title>
-<<<<<<< HEAD
-        <Heroes></Heroes>
-=======
         <v-card-actions>
             <v-spacer></v-spacer>
             <v-col>
@@ -371,7 +197,6 @@
               <v-btn color="green darken-1" @click="dialogoHeroes = false">Cancelar</v-btn>
             </v-col>
           </v-card-actions>
->>>>>>> 815f9f11f7c14285ffcf95271823dd199192e12f
       </v-card>
     </v-dialog>
   </v-container>
